c2655e2b88e9e518144630d54aa660fb03b98840
Victor Busolin <victorbusolin@alunos.utfpr.edu.br>
2024-01-22 13:18:26 -0300

    feat: tests and logic

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-import fastify from 'fastify';
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP SERVER RUNNING')
+  })
